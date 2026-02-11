4bf027d9aeaf4e003f8df56c25b323f6983c0c0d
yfree1232-afk <yfree1232@gmail.com>
2026-02-11 11:47:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,3 +44,18 @@ def hello_world():
 
 if __name__ == "__main__":
     app.run()
+from flask import Flask
+import os
+import threading
+
+app_web = Flask(__name__)
+
+@app_web.route("/")
+def home():
+    return "Bot Running ✅"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app_web.run(host="0.0.0.0", port=port)
+
+threading.Thread(target=run_web).start()
