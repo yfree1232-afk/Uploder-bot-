c21b3cc6645ced5718f1fdeefa0d5c4cde5f6ad3
yfree1232-afk <yfree1232@gmail.com>
2026-02-11 11:32:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1764,3 +1764,17 @@ if __name__ == "__main__":
 
 
 bot.run()
+
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host="0.0.0.0", port=10000)
+
+Thread(target=run).start()
