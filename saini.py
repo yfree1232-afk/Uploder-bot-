57685831ba067fd45c9adab72c997ab984f43bf2
yfree1232-afk <yfree1232@gmail.com>
2026-06-07 18:01:48 +0530

    fix: Add --hls-use-mpegts to yt-dlp so HLS works without ffmpeg

--- saini.py
+++ saini.py
@@ -233,7 +233,7 @@ def time_name():
 
 
 async def download_video(url, cmd, name):
-    download_cmd = f'{cmd} -R 25 --fragment-retries 25 --external-downloader aria2c --downloader-args "aria2c: -x 16 -j 32"'
+    download_cmd = f'{cmd} -R 25 --fragment-retries 25 --hls-use-mpegts --external-downloader aria2c --downloader-args "aria2c: -x 16 -j 32"'
     global failed_counter
     print(download_cmd)
     logging.info(download_cmd)
